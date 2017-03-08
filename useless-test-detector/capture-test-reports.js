1ad89c00c243817953fac54c136d3f3549de9972
Vivekanand Ramakrishnan <vramakr2@ncsu.edu>
2017-03-08 11:36:41 -0500

    Formatted capture-test-reports

--- useless-test-detector/capture-test-reports.js
+++ useless-test-detector/capture-test-reports.js
@@ -25,15 +25,14 @@ for(var i = 0 ; i < dirs.length; i++)
     }
 }
 
-console.log('\nBuild-Tools | Useless Test Detector : STARTED');
+var failedTC =0;
+var passedTC =0;
 
 //var nextBuildNumberFile = '/var/lib/jenkins/jobs/' + jobName + '/nextBuildNumber';
 var nextBuildNumberFile = '/Users/vivekanr/workspace/' + jobName + '/iTrust/target/nextBuildNumber';
 //console.log(nextBuildNumberFile);
 
 var nextBuildNumber = fs.readFileSync(nextBuildNumberFile, "utf8").toString().trim();
-
-console.log("Fetching SureFire Test Reports for Job: " + jobName + " Build No: " + nextBuildNumber + " using Build-Tools | Useless Test Detector\n");
 var buildDir = reportsPath + nextBuildNumber + "/";
 
 // Create reports/builds directory    
@@ -47,6 +46,82 @@ if (!fs.existsSync(buildDir)){
     fs.mkdirSync(buildDir);
 }
 
+function report(failedTestsCount, successfulTestsCount){
+    var allTestCases = failedTestsCount + successfulTestsCount;
+    console.log('Passed Testcases : ' + successfulTestsCount + '\t Failed Testcases : ' + failedTestsCount + '\t Total Testcases : ' + allTestCases +'\n');
+    console.log('Build-Tools | Useless Test Detector : COMPLETED\n');
+}
+
+function processFile(testReport, callback) {
+
+    callback(testReport);
+
+}
+
+function collectStats(testReport) {
+
+    fs.readFile(testReport, function(err, data) {
+        parser.parseString(data, function (err, result) {
+            var allTestCases = result.testsuite.testcase;
+            var failedTests = [];
+            var successfulTests = [];
+            
+            for(var i = 0; i < allTestCases.length; i++) {
+                if(allTestCases[i].failure) {
+                    failedTests.push(allTestCases[i]);
+                    failedTC += 1;
+                } else {
+                    successfulTests.push(allTestCases[i]);
+                    passedTC += 1;
+                }
+            }
+
+            //console.log("-------------------- " + allTestCases[0].$.classname + " -------------------- ");
+            
+            //console.log("#################### FAILED TESTS #########################\n");
+            for (var i = 0; i < failedTests.length; i++) {
+                //console.log(failedTests[i].$.classname + ", " + failedTests[i].$.name + " Failed: TRUE" );
+
+                var failedFileName = buildDir + 'failed.txt';
+                var text = failedTests[i].$.classname + ", " + failedTests[i].$.name + "\n";
+
+                fs.appendFile(failedFileName, text, function (err) {
+                   if (err) {
+                      return console.error(err);
+                   }
+                });
+
+            }
+
+            //console.log("#################### SUCCESSFUL TESTS #########################\n");
+            for (var i = 0; i < successfulTests.length; i++) {
+                //console.log(successfulTests[i].$.classname + ", " + successfulTests[i].$.name + " Failed: FALSE" );
+
+
+                var successFileName = buildDir + 'success.txt';
+                var text = successfulTests[i].$.classname + ", " + successfulTests[i].$.name + "\n";
+
+                fs.appendFile(successFileName, text, function (err) {
+                   if (err) {
+                      return console.error(err);
+                   }
+                });
+            }
+            
+        });
+        
+        calls.pop();
+        if(calls.length === 0) {
+            report(failedTC,passedTC);
+        }
+
+    });
+
+}
+
+console.log('\nBuild-Tools | Useless Test Detector : STARTED\n');
+console.log("Fetching SureFire Test Reports for Job: " + jobName + " Build No: " + nextBuildNumber);
+var calls =[];
 // Parse sure-fire .xml Files 
 recursive(projectPath, ['*.txt'], function (err, allFiles) {
 
@@ -57,59 +132,9 @@ recursive(projectPath, ['*.txt'], function (err, allFiles) {
             var testReport = allFiles[id];
             //console.log(testReport);
 
-            fs.readFile(testReport, function(err, data) {
-                parser.parseString(data, function (err, result) {
-                    var allTestCases = result.testsuite.testcase;
-                    var failedTests = [];
-                    var successfulTests = [];
-                    
-                    for(var i = 0; i < allTestCases.length; i++) {
-                        if(allTestCases[i].failure) {
-                            failedTests.push(allTestCases[i]);
-                        } else {
-                            successfulTests.push(allTestCases[i]);
-                        }
-                    }
-
-                    //console.log("-------------------- " + allTestCases[0].$.classname + " -------------------- ");
-                    
-                    //console.log("#################### FAILED TESTS #########################\n");
-                    for (var i = 0; i < failedTests.length; i++) {
-                        //console.log(failedTests[i].$.classname + ", " + failedTests[i].$.name + " Failed: TRUE" );
-
-                        var failedFileName = buildDir + 'failed.txt';
-                        var text = failedTests[i].$.classname + ", " + failedTests[i].$.name + "\n";
-
-                        fs.appendFile(failedFileName, text, function (err) {
-                           if (err) {
-                              return console.error(err);
-                           }
-                        });
-
-                    }
-
-                    //console.log("#################### SUCCESSFUL TESTS #########################\n");
-                    for (var i = 0; i < successfulTests.length; i++) {
-                        //console.log(successfulTests[i].$.classname + ", " + successfulTests[i].$.name + " Failed: FALSE" );
-
-
-                        var successFileName = buildDir + 'success.txt';
-                        var text = successfulTests[i].$.classname + ", " + successfulTests[i].$.name + "\n";
-
-                        fs.appendFile(successFileName, text, function (err) {
-                           if (err) {
-                              return console.error(err);
-                           }
-                        });
-                    }
-                    
-                });
-            });
+            calls.push(1);
+            processFile(testReport, collectStats);
 
         }
     }
 });
-
-console.log('\nBuild-Tools | Useless Test Detector : COMPLETED');
-
-
